c88cc7c2888d3fe78eefc2d1d80393072d582b19
周龙 <5059521+xlwmin@users.noreply.github.com>
2019-06-03 16:42:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ if [ "$VER" = "latest" ]; then
 else
   wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
   unzip v2ray.zip 
-  mv /v2raybin/v2ray-v$VER-linux-64/v2ray .
-  mv /v2raybin/v2ray-v$VER-linux-64/v2ctl .
-  mv /v2raybin/v2ray-v$VER-linux-64/geoip.dat .
-  mv /v2raybin/v2ray-v$VER-linux-64/geosite.dat .
+  mv /v2raybin/v2ray/v2ray .
+  mv /v2raybin/v2ray/v2ctl .
+  mv /v2raybin/v2ray/geoip.dat .
+  mv /v2raybin/geosite.dat .
 fi
 
 chmod +x v2ray
